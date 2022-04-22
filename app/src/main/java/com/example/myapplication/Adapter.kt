9556c6af26d858ee32e818f023ad7db1a61b1620
zahhard <za.darardoust@gmail.com>
2022-04-22 05:42:58 +0430

    clickHandler added

--- app/src/main/java/com/example/myapplication/Adapter.kt
+++ app/src/main/java/com/example/myapplication/Adapter.kt
@@ -6,32 +6,37 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class Adapter
-class CustomAdapter(private val dataSet: List<Doctor>) :
+typealias clickHandler =  ((doctor : Doctor) -> Unit)
+class CustomAdapter(
+    private val dataSet: List<Doctor>,
+    var clickHandler: clickHandler
+) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView
-        val textViewNum: TextView
-        val textViewAddress: TextView
+    class ViewHolder(view: View, var clickHandler: clickHandler) :
+        RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+        var btnOnClick = view.findViewById<TextView>(R.id.btn_show)
+//      val textViewAddress: TextView
 
-        init {
-            textView = view.findViewById(R.id.textView)
-            textViewNum = view.findViewById(R.id.tv_num)
-            textViewAddress = view.findViewById(R.id.tv_city)
+
+        fun bind(doctor: Doctor, clickHandler: clickHandler){
+            btnOnClick.setOnClickListener{
+                clickHandler(doctor)
+            }
         }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.item, viewGroup, false)
-        return ViewHolder(view)
+        return ViewHolder(view, clickHandler)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         viewHolder.textView.text = dataSet[position].name
-        viewHolder.textViewNum.text = dataSet[position].phoneNumber.toString()
-        viewHolder.textViewAddress.text = dataSet[position].adress
+        viewHolder.bind(dataSet[position] , clickHandler)
+//        viewHolder.textViewAddress.text = dataSet[position].adress
     }
 
     override fun getItemCount() = dataSet.size
