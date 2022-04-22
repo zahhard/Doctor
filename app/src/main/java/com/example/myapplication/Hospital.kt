61602c6c906f2ac1f72e32741d333bf7caba721d
zahhard <za.darardoust@gmail.com>
2022-04-22 05:43:28 +0430

    test data updated

--- app/src/main/java/com/example/myapplication/Hospital.kt
+++ app/src/main/java/com/example/myapplication/Hospital.kt
@@ -12,9 +12,15 @@ object Hospital {
         val d1 = Doctor("سارا بهرامی", 1, 332210, "تهران")
         val d2 = Doctor("کوثر سپهسالار", 2, 323565, "تبریز")
         val d3 = Doctor("آراز کبریان", 3, 355598, "بوشهر")
+        val d6 = Doctor("کیانا خوشروی", 4, 332210, "مشهد")
+        val d4 = Doctor("زهره صمدی", 5, 323565, "شیراز")
+        val d5 = Doctor("سمانه نامدار", 6, 355598, "سراب")
         doctorList.add(d1)
         doctorList.add(d2)
         doctorList.add(d3)
+        doctorList.add(d4)
+        doctorList.add(d5)
+        doctorList.add(d6)
     }
 }
 
